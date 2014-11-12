ebc9cf92f13c7aa2a8f67edb7fb23ef20abf888e
corktw <yugivinc@hotmail.com>
2014-11-12 18:50:45 +0800

    Added Accordions

--- js/scripts.js
+++ js/scripts.js
@@ -38,4 +38,41 @@ $( function () {
 
         $this.addClass( "animated " +$this.text());
      } )
+
+     /*================================================*
+      * Tabs.
+      *================================================*/
+
+     $( ".tabs-links a" ).on( "click", function ( e ) {
+        e.preventDefault();
+
+        // Get the target tab
+        var newTab = $( this ).attr( "href" );
+
+        // Show the targeted tab content while hiding the rest
+        $( newTab ).show().siblings().hide();
+
+        // Add the active class to the parent list item while removing it from the rest.
+        $( this ).parent( "li" ).addClass( "active" ).siblings().removeClass( "active" );
+       } );
+
+     /*================================================*
+      * Accordions.
+      *================================================*/
+     
+     $( ".accordion-element-title" ).on( "click", function(e) {
+        e.preventDefault;
+        
+        // Get the target Accordion
+        var targetAccordion = $( this ).attr( "href" );
+
+        // Collapse all accordion elements.
+        $( ".accordion-element-title" ).removeClass( "active" );
+        $( ".accordion-element-content" ).hide();
+
+        // Show the targeted element only.
+        $( this ).addClass( "active" );
+        $( targetAccordion ).show();
+
+     }); 
 } );
